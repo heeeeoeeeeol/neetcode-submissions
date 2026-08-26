3d99836ed47a9dbf09c6a894463fd349be41ed14
heeeeoeeeeol <60788888+heeeeoeeeeol@users.noreply.github.com>
2026-08-26 15:37:08 -0700

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        subset = []
+        s = 0
+        candidates.sort()
+
+        def findSub(i):
+            nonlocal res, subset, s
+            if s == target: 
+                res.append(subset[:]) 
+                return
+
+            for j in range(i, len(candidates)):
+                if s + candidates[j] > target: return
+                if j > i and candidates[j-1] == candidates[j]: continue
+
+                subset.append(candidates[j])
+                s+=candidates[j]
+                findSub(j+1)
+                subset.pop()
+                s-=candidates[j]
+
+        findSub(0)
+        return res
