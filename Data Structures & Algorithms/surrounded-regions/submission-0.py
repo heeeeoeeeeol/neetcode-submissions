013db88a4ca8c91336ed0f0ed328ad444a36e897
heeeeoeeeeol <60788888+heeeeoeeeeol@users.noreply.github.com>
2026-09-01 17:30:37 -0700

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        vis, ROWS, COLS = set(), len(board), len(board[0])
+        def dfs(r,c):
+            if not(0<=r<ROWS and 0<=c<COLS) or board[r][c]=="X" or (r,c) in vis: return
+            vis.add((r,c))
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c-1)
+            dfs(r,c+1)
+
+        for r in range(ROWS): 
+            if board[r][0] == "O": dfs(r,0)
+            elif board[r][-1] == "O": dfs(r,COLS-1)
+        for c in range(COLS): 
+            if board[0][c] == "O": dfs(0,c)
+            elif board[-1][c] == "O": dfs(ROWS-1,c)
+
+        for r in range(1, ROWS-1):
+            for c in range(1, COLS-1):
+                if board[r][c]=="O" and (r,c) not in vis:
+                    board[r][c] = "X"
