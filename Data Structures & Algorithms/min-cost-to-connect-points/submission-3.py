7179133c6e8b95b18da4e739c1f1b8e400d4478d
heeeeoeeeeol <60788888+heeeeoeeeeol@users.noreply.github.com>
2026-09-04 16:48:45 -0700

    Add: min-cost-to-connect-points - submission-3

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-3.py
@@ -0,0 +1,36 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [0]*n
+
+    def find(self, node):
+        if self.parent[node] != node:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2: return False
+        if self.size[p1] >= self.size[p2]: p2, p1 = p1, p2
+        self.parent[p1] = p2
+        self.size[p2] += self.size[p1]
+        return True
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int: 
+        uf = DSU(len(points))
+        l = []
+        for i in range(len(points)):
+            for j in range(i+1,len(points)): l.append((abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]),i,j))
+     
+        l.sort(key=lambda x:x[0])
+        edges, res = 0, 0
+        for p in l:
+            if uf.union(p[1],p[2]): 
+                res += p[0]
+                edges += 1
+            if edges == len(points)-1: return res
+
+        return 0
+
+                
